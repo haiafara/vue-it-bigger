57e400d0b330affcd80ee1ad5a6a20ba4321533d
Janos Rusiczki <janos.rusiczki@gmail.com>
2020-10-05 10:33:46 +0000

    Update specs and coverage

--- specs/LightBox.spec.js
+++ specs/LightBox.spec.js
@@ -127,6 +127,120 @@ describe('LightBox', () => {
     test('renders one active thumbnail element', () => {
       expect(wrapper.find('div.vib-thumbnail-active').exists()).toBe(true)
     })
+
+    describe('keypresses', () => {
+      describe('left arrow', () => {
+        beforeEach(() => {
+          wrapper.vm.previousImage = jest.fn()
+          document.dispatchEvent(new KeyboardEvent('keydown', { 'keyCode': 37 }))
+        })
+
+        test('calls previousImage', () => {
+          expect(wrapper.vm.previousImage).toHaveBeenCalled()
+        })
+      })
+
+      describe('right arrow', () => {
+        beforeEach(() => {
+          wrapper.vm.nextImage = jest.fn()
+          document.dispatchEvent(new KeyboardEvent('keydown', { 'keyCode': 39 }))
+        })
+
+        test('calls nextImage', () => {
+          expect(wrapper.vm.nextImage).toHaveBeenCalled()
+        })
+      })
+
+      describe('esc', () => {
+        beforeEach(() => {
+          wrapper.vm.closeLightBox = jest.fn()
+          document.dispatchEvent(new KeyboardEvent('keydown', { 'keyCode': 27 }))
+        })
+
+        test('calls closeLightBox', () => {
+          expect(wrapper.vm.closeLightBox).toHaveBeenCalled()
+        })
+      })
+    })
+
+    describe('mouse movement', () => {
+      beforeEach(() => {
+        jest.useFakeTimers()
+      })
+
+      describe('moving the mouse', () => {
+        describe('when controls are shown', () => {
+          beforeEach(() => {
+            wrapper.setData({ controlsHidden: false })
+            wrapper.findComponent({ ref: 'container' }).trigger('mousemove')
+          })
+
+          test('leaves controlsHidden to be false', () => {
+            expect(wrapper.vm.controlsHidden).toBe(false)
+          })
+
+          test('setTimeout is called once', () => {
+            expect(setTimeout).toHaveBeenCalledTimes(1)
+          })
+
+          describe('when timeout is reached', () => {
+            beforeEach(() => {
+              jest.runOnlyPendingTimers()
+            })
+
+            test('controls are hidden', () => {
+              expect(wrapper.vm.controlsHidden).toBe(true)
+            })
+          })
+        })
+
+        describe('when controls are hidden', () => {
+          beforeEach(() => {
+            wrapper.setData({ controlsHidden: true })
+            wrapper.findComponent({ ref: 'container' }).trigger('mousemove')
+          })
+
+          test('sets controlsHidden to false', () => {
+            expect(wrapper.vm.controlsHidden).toBe(false)
+          })
+
+          test('setTimeout is called once', () => {
+            expect(setTimeout).toHaveBeenCalledTimes(1)
+          })
+        })
+      })
+    })
+
+    describe('transitions', () => {
+      beforeEach(() => {
+        wrapper.setData({ imageTransitionName: 'test' })
+      })
+
+      describe('.enableImageTransition', () => {
+        beforeEach(() => {
+          wrapper.vm.handleMouseActivity = jest.fn()
+          wrapper.vm.enableImageTransition()
+        })
+
+        test('calls handleMouseActivity', () => {
+          expect(wrapper.vm.handleMouseActivity).toHaveBeenCalled()
+        })
+
+        test('sets imageTransitionName correctly', () => {
+          expect(wrapper.vm.imageTransitionName).toBe('vib-image-transition')
+        })
+      })
+
+      describe('.disableImageTransition', () => {
+        beforeEach(() => {
+          wrapper.vm.disableImageTransition()
+        })
+
+        test('sets imageTransitionName correctly', () => {
+          expect(wrapper.vm.imageTransitionName).toBe('vib-image-no-transition')
+        })
+      })
+    })
   })
 
   describe('given one video in the media array', () => {
@@ -244,51 +358,6 @@ describe('LightBox', () => {
 
       test('removeEventListener is called on the container 3 times', () => {
         expect(container.removeEventListener).toHaveBeenCalledTimes(3)
-      })
-    })
-  })
-
-  describe('keypresses', () => {
-    let wrapper
-
-    beforeEach(() => {
-      wrapper = mount(LightBox, {
-        propsData: {
-          media: mediaWithOneImage
-        }
-      })
-    })
-
-    describe('left arrow', () => {
-      beforeEach(() => {
-        wrapper.vm.previousImage = jest.fn()
-        document.dispatchEvent(new KeyboardEvent('keydown', { 'keyCode': 37 }))
-      })
-
-      test('calls previousImage', () => {
-        expect(wrapper.vm.previousImage).toHaveBeenCalled()
-      })
-    })
-
-    describe('right arrow', () => {
-      beforeEach(() => {
-        wrapper.vm.nextImage = jest.fn()
-        document.dispatchEvent(new KeyboardEvent('keydown', { 'keyCode': 39 }))
-      })
-
-      test('calls nextImage', () => {
-        expect(wrapper.vm.nextImage).toHaveBeenCalled()
-      })
-    })
-
-    describe('esc', () => {
-      beforeEach(() => {
-        wrapper.vm.closeLightBox = jest.fn()
-        document.dispatchEvent(new KeyboardEvent('keydown', { 'keyCode': 27 }))
-      })
-
-      test('calls closeLightBox', () => {
-        expect(wrapper.vm.closeLightBox).toHaveBeenCalled()
       })
     })
   })
